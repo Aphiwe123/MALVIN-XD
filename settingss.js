3792015e66439ac4ad9fee152a3747cece0a6709
Aphiwe123 <mzizimathenjwa@gmail.com>
2025-08-06 02:00:18 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,9 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "",  malvin~I2c33bwL#GT5LEgofaEWi3vzMB2VTrjrePnFFX-MgABljjxV-OOw// add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "27734593686
+    ", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
